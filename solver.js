dc7530ed3536de3a63c377af72e6a2d58eceb538
Bendegúz Székács <bendeguz.szekacs@gmail.com>
2022-05-25 20:26:03 +0200

    a

--- solver.js
+++ solver.js
@@ -1,12 +1,26 @@
 //solves an unfinished Sudoku
 class Solver {
+
     constructor(board, size) {
+
         this.board = board;
         this.size = size;
         if(this.checkMistakes()) {throw new Error('Board has mistake');}
-        this.solution = new Array(size).fill(new Array(size));
+
+        this.solution = [[]];
+        for (let i = 1; i < size; i++) {this.solution.push([])}
+        
         if(!this.solve()) {throw new Error('No solution');}
+        else {
+            for(let r = 0; r < this.size; r++) {
+                for(let c = 0; c < this.size; c++) {
+                    let n = this.board[r][c];
+                    if(n !== 0) {this.solution[r][c] = n;}
+                }
+            }
+        }
     }
+
 
     //recursive backtarcking method
     solve() {
@@ -15,32 +29,36 @@ class Solver {
         let r, c;
         let gotCell = false;
         for(r = 0; r < this.size && !gotCell; r++) {
-            for (c = 0; c < this.size && !gotCell; c++) {
-                if(this.board[r][c] !== 0) {gotCell = true;}
+            for(c = 0; c < this.size && !gotCell; c++) {
+                if(this.board[r][c] === 0) {gotCell = true;}
             }
         }
         if(!gotCell) {return true;} //returns true if there are no open cells
+        r--;
+        c--;
 
         //goes through the usable numbers
         let usable = this.getUsable(r, c);
-        usable.forEach(n => {
+        for (const n of usable) {
             this.board[r][c] = n;
             if(this.solve()) {
                 this.solution[r][c] = n;
-                console.log(n);
                 return true;
             }
-        });
+        }
         this.board[r][c] = 0;
         return false; //returns false if all usable numbers are wrong
 
     }
 
+
     //returns the usable numbers for a given cell
     getUsable(x, y) {
+
         let usable = [];
         for(let n = 1; n < this.size + 1; n++) {
             let isUsable = true;
+
             //check x, y axis
             for(let r = 0; r < this.size && isUsable; r++) {
                 if(this.board[r][y] === n) {isUsable = false;}
@@ -64,8 +82,12 @@ class Solver {
     }
 }
 
+
+
+
+let solv;
 try {
-    let solv = new Solver([
+    /*let solv = new Solver([
         [6, 3, 5, 0, 8, 0, 1, 0, 0],
         [0, 0, 0, 5, 3, 0, 0, 4, 0],
         [0, 0, 7, 0, 0, 1, 3, 0, 0],
@@ -76,7 +98,13 @@ try {
         [7, 0, 0, 0, 0, 5, 0, 0, 8],
         [3, 0, 8, 0, 0, 0, 0, 9, 0]
     ], 9);
-    console.log(solv.solution);
+    console.log(solv.solution);*/
+    solv = new Solver([
+        [1, 0, 0, 4],
+        [0, 4, 0, 0],
+        [2, 1, 0, 3],
+        [0, 0, 0, 0]
+    ], 4);
 } catch (e) {
    console.error(e);
 }
